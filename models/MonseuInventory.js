ed2e5d395f63da5739f256e0a1e473ff4ae2f2bb
as known as 3.14 <happy.535240127@stu.untar.ac.id>
2025-04-24 22:52:09 +0700

    Create MonseuInventory.js

---
+++ models/MonseuInventory.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const monseuInventorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    text1: {
+      type: String,
+      required: true,
+    },
+    text2: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const MonseuInventory = mongoose.model(
+  "MonseuInventory",
+  monseuInventorySchema
+);
+
+module.exports = MonseuInventory;
