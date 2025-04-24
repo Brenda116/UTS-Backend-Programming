bf843948d07335a7d8e3b6dbf52150fb30fbf2a5
Kathleen Klavinka <kathleen.535240125@stu.untar.ac.id>
2025-04-24 17:51:50 +0700

    Update stockRoutes.js

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/User");
-const controller = require("../controllers/stockController");
-const { requireAuth, requireAdmin } = require("../middlewares/authMiddleware");
+const express = require("express"); // Import express 
+const router = express.Router(); // Membuat instance router
+const User = require("../models/User"); // Import model user 
+const controller = require("../controllers/stockController"); // Import controller (CRUD) untuk stock barang
+const { requireAuth, requireAdmin } = require("../middlewares/authMiddleware"); // Import middleware untuk aitentikasi admin
+
+// Route GET utama untuk menampilkan data stock
+// Hanya dapat diakses oleh admin yang sudah login
 router.get("/", requireAuth, requireAdmin, controller.index);
+
+// Route POST untuk melakuakn update data stock
+// Hanya dapat diakses oleh admin yang sudah login
 router.post("/update", requireAuth, requireAdmin, controller.update);
 
+// Ekspor router agar bisa digunakan di file utama aplikasi
 module.exports = router;
